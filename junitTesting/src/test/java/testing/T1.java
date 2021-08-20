22c35028d29fbe4eb426caf061223b46544785bd
kr-abhishek3031 <kumar.abhishek3031@gmail.com>
2021-08-20 14:11:06 +0530

    jUnit initiation

---
+++ junitTesting/src/test/java/testing/T1.java
@@ -0,0 +1,16 @@
+package testing;
+
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class T1 {
+
+        @Test
+        void checktest(){
+            assertTrue(true);
+        }
+}
+
+
